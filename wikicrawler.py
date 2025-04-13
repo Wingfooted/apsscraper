b45ad7de0c884e28bc94361c794e06b74b33efb8
Wingfooted <Wingfooted3@gmail.com>
2025-04-13 22:40:28 +1000

    adding all gov agencies

--- wikicrawler.py
+++ wikicrawler.py
@@ -1,26 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
+import tqdm
+import time
+import urllib
+
+# get_government_agencies -> process_agencies
 
 
 def get_links(url):
+    netloc = urllib.parse.urlparse(url).netloc
     response = requests.get(url)
     links = []
-
+    external, internal = set(), set()
     if response.status_code == 200:
         text = response.text
         soup = BeautifulSoup(text, 'html.parser')
-        links = [link.get('href') for link in soup.find_all('a')]
-        return links
+        for l in soup.find_all('a'):
+            link = l.get('href')
+            if not link:
+                continue
 
+            if link[0] == '/':
+                internal.add('https://'+netloc+link)
+            else:
+                external.add(link)
     else:
         print("Error", response.status_code, url)
 
-    return links
+    return external, internal
+
+
+def get_governemnt_agencies():
+    agencies = set()
+    e, i = get_links('https://en.wikipedia.org/wiki/List_of_Australian_Government_entities')
+
+    for internal in tqdm.tqdm(i):
+        external, _ = get_links(internal)
+        for e in external:
+            if ".gov.au" in urllib.parse.urlparse(e).netloc:
+                agencies.add(e)
+
+    return agencies
+
+
+def process_agencies(filename):
+    with open(filename, 'r') as ag:
+        links = [
+            f"https://{urllib.parse.urlparse(l.replace("\n", "")).netloc}\n"
+            for l in ag.readlines()]
+        links = set(links)
+
+    with open(f"{filename.replace('.txt', '')}_processed.txt", "w") as agp:
+        for l in links:
+            agp.write(l)
 
 
 if __name__ == '__main__':
-
-    links = get_links('https://en.wikipedia.org/wiki/List_of_Australian_Government_entities')
-
-    for l in links:
-        print(l)
+    process_agencies("ag.txt")
